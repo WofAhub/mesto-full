f75387abac25dd48198b395812268ac76cdb74fa
WofA <wofamyworld@gmail.com>
2023-06-28 01:36:30 +0300

    Корректировки по env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,9 @@
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-const { checkToken } = require('../utils/jwtAuth');
 const UnauthorizedError = require('../errors/UnauthorizedError');
+
+// const, связанный с env
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => { // eslint-disable-line
   const { authorization } = req.headers;
@@ -14,7 +17,7 @@ module.exports = (req, res, next) => { // eslint-disable-line
   let payload;
 
   try {
-    payload = checkToken(token);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
     req.user = {
       _id: new mongoose.Types.ObjectId(payload._id),
     };
