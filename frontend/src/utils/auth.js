46810def037fa9f853bafbe03a6d2cd5ebecae0c
WofA <wofamyworld@gmail.com>
2023-07-02 18:59:25 +0300

    Авотризация

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,59 +1,60 @@
 export const BASE_URL = 'https://api.wofamesto.nomoreparties.sbs';
 
-// универсальная функция
-const makeRequest = (url, method, body, token) => {
-    const options = {
-        url,
-        method,
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json"
-        }
+// проверка ответа с сервера
+function checkAnswerFromServer(res) {
+    if (res.ok) {
+        return res.json();
+    } else {
+        return Promise.reject(`Ошибка: ${res.status}`);
     }
-
-    if (body) {
-        options.body = JSON.stringify(body);
-    }
-
-    if (token === undefined) {
-        options.headers.Authorization = `Bearer ${token}`
-    }
-
-    return fetch(`${BASE_URL}${url}`, options)
-    .then((res) => {
-        if (res.ok) {
-            return res.json();
-        } else {
-            console.log("Ошибка в makeRequest, then");
-            return Promise.reject(`Ошибка: ${res.status}`);
-        }
-    })
 }
 
 // регистрация
 export const register = (email, password) => {
-    return makeRequest(
-        '/signup', 
-        'POST', 
-        {email, password}
-    )
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(res => checkAnswerFromServer(res))
+        .then((res) => {
+            return res;
+        })
+        .catch((err) => {
+            console.log(`Ошибка в register, auth: ${err}`)
+        })
 }
 
 // логин
 export const login = (email, password) => {
-    return makeRequest(
-        '/signin', 
-        'POST', 
-        {email, password}
-    )
+    return fetch(`${BASE_URL}/signin`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(res => checkAnswerFromServer(res))
+        .then((data) => {
+            localStorage.setItem('jwt', data.jwt);
+            return data;
+        })
 };
 
-// получение меня
-export const getContent = (token) => {
-    return makeRequest(
-        '/users/me', 
-        'GET',
-        null, 
-        token
-    )
+// проверка токена
+export const checkToken = (token) => {
+    return fetch(`${BASE_URL}/users/me`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+    })
+        .then(res => checkAnswerFromServer(res))
+        .then(data => data)
 };
